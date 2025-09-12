8d29f39ac9ad643d886b71ebe8bb1edd84bbbe24
Kezia Santnn <keziasantnn@gmail.com>
2025-09-12 16:02:09 -0300

    ajuste mobile

--- src/App.tsx
+++ src/App.tsx
@@ -57,124 +57,108 @@ function App() {
         </p>
       </article>
       <section className="box-insta">
-        <article className="left">
-          <h2 className="title2">
-            Conecte-se <br />
-            Mantenha Contato
-          </h2>
-          <p className="text2">
-            Acompanhe tudo o que Deus tem feito em nossa igreja e faça parte
-            dessa comunhão! <br />
-            💙 Siga nosso Instagram ✨
-            <p>
-              <a href="https://www.instagram.com/ib.marco2" target="_blank">
-                @ib.marco2
-              </a>
-            </p>
+        <h2 className="title2">
+          Conecte-se <br />
+          Mantenha Contato
+        </h2>
+        <p className="text2">
+          Acompanhe tudo o que Deus tem feito em nossa igreja e faça parte dessa
+          comunhão! <br />
+          💙 Siga nosso Instagram ✨
+        </p>
+        <div className="container-img-insta">
+          <figure className="img-insta">
+            <a
+              href="https://www.instagram.com/ib.marco2"
+              target="_blank"
+              rel="noreferer noopener"
+            >
+              <img src="assets/instacode.png" alt="QR CODE" />
+            </a>
+          </figure>
+          <p>
+            <a href="https://www.instagram.com/ib.marco2" target="_blank">
+              @ib.marco2
+            </a>
           </p>
-          <p className="text2">
-            Queremos estar perto de você, orando e intercedendo pela sua vida.
-            Se precisar de uma palavra, um conselho ou um pedido de oração,
-            entre em contato! Estamos aqui para caminhar juntos em fé. 📖 <br />
-            "Orem uns pelos outros para serem curados. A oração de um justo é
-            poderosa e eficaz."
-          </p>
-        </article>
-        <figure className="img-insta">
-          <a
-            href="https://www.instagram.com/ib.marco2"
-            target="_blank"
-            rel="noreferer noopener"
-          >
-            <img src="assets/instacode.png" alt="QR CODE" />
-          </a>
-        </figure>
+        </div>
+        <p className="text2">
+          Queremos estar perto de você, orando e intercedendo pela sua vida. Se
+          precisar de uma palavra, um conselho ou um pedido de oração, entre em
+          contato! Estamos aqui para caminhar juntos em fé. 📖 <br />
+          "Orem uns pelos outros para serem curados. A oração de um justo é
+          poderosa e eficaz."
+        </p>
       </section>
       <nav className="box-proj">
         <CarouselProject isOpen={openProject} onClose={handleCloseProject} />
-        <div className="box-img">
-          <img
-            className="proj-img"
-            src="assets/entre-maes.png"
-            alt="logo entre maes"
-            height="150px"
-            width="150px"
-          />
-          <img
-            className="proj-img"
-            src="assets/estudo-casais.png"
-            alt="logo casais"
-            height="150px"
-            width="150px"
-          />
-          <img
-            className="proj-img"
-            src="assets/kravmaga.png"
-            alt="logo KRAVMAGA"
-            height="150px"
-            width="150px"
-          />
-          <img
-            className="proj-img"
-            src="assets/homens.png"
-            alt="logo homens"
-            height="150px"
-            width="150px"
-          />
-          <img
-            className="proj-img"
-            src="assets/mulheres.jpg"
-            alt="logo Mulheres"
-            height="150px"
-            width="150px"
-          />
-          <img
-            className="proj-img"
-            src="assets/Pgm.png"
-            alt="logo pgm"
-            height="150px"
-            width="150px"
-          />
-          <img
-            className="proj-img"
-            src="../public/assets/kids.png"
-            alt="logo kids"
-            height="150px"
-            width="150px"
-          />
-        </div>
-        <div className="box-tex">
-          <h2 className="title2">Projetos</h2>
-          <p className="text2">
-            Nossa igreja tem a missão de conectar vidas a Cristo de maneira
-            transformadora. <br />
-            Por isso, desenvolvemos projetos que atendem diferentes públicos e
-            necessidades, proporcionando crescimento espiritual, comunhão e
-            bem-estar.
-            <br />
-            Conheça nossos projetos:
-          </p>
-          <button className="button" onClick={handleOpenProject}>
-            Saiba Mais
-          </button>
+        <h2 className="title2">Projetos</h2>
+        <div className="container-project">
+          <div className="box-img">
+            <img
+              className="proj-img"
+              src="assets/entre-maes.png"
+              alt="logo entre maes"
+            />
+            <img
+              className="proj-img"
+              src="assets/estudo-casais.png"
+              alt="logo casais"
+            />
+            <img
+              className="proj-img"
+              src="assets/kravmaga.png"
+              alt="logo KRAVMAGA"
+            />
+            <img
+              className="proj-img"
+              src="assets/homens.png"
+              alt="logo homens"
+            />
+            <img
+              className="proj-img"
+              src="assets/mulheres.jpg"
+              alt="logo Mulheres"
+            />
+            <img className="proj-img" src="assets/Pgm.png" alt="logo pgm" />
+            <img
+              className="proj-img"
+              src="../public/assets/kids.png"
+              alt="logo kids"
+            />
+          </div>
+          <div className="box-tex">
+            <p className="text2">
+              Nossa igreja tem a missão de conectar vidas a Cristo de maneira
+              transformadora. <br />
+              Por isso, desenvolvemos projetos que atendem diferentes públicos e
+              necessidades, proporcionando crescimento espiritual, comunhão e
+              bem-estar.
+              <br />
+              Conheça nossos projetos:
+            </p>
+            <button className="button" onClick={handleOpenProject}>
+              Saiba Mais
+            </button>
+          </div>
         </div>
       </nav>
       <div className="final">
-        <section>
+        <section style={{ padding: "20px" }}>
           <div
             style={{ display: "flex", alignItems: "center", marginBottom: 40 }}
           >
             <img
+              style={{ width: "5%" }}
               src="assets/logo-marcoII.png"
               alt="Logo Igreja Batista de Marco II"
-              style={{ height: 60, verticalAlign: "middle", marginRight: 10 }}
             />
             <span className="subtitle"> Igreja Batista de Marco II</span>
           </div>
           <div
             style={{
               display: "flex",
-              justifyContent: "space-between",
+              justifyContent: "center",
               alignItems: "center",
               flexWrap: "wrap",
             }}
